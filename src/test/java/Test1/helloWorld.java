a88183e9cc40e364607d29927a46701c54cb7223
Lingesh Raja <Lingesh Raja@192.168.12.72>
2025-06-25 16:14:31 -0400

    First commit

---
+++ src/test/java/Test1/helloWorld.java
@@ -0,0 +1,24 @@
+package Test1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class helloWorld {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://amazon.com");
+		
+		WebElement searchTxtBox = driver.findElement(By.name("field-keywords"));
+		searchTxtBox.sendKeys("Women handbags");
+		Thread.sleep(2000);
+		
+		driver.close();
+		
+	}
+
+}
